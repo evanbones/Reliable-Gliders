3fcf91b05c47a69c719355188b33c0fc7551edac
evanbones <74276616+evanbones@users.noreply.github.com>
2026-05-02 03:20:42 -0700

    tweaks

--- fabric/src/main/java/com/evandev/reliable_gliders/client/ReliableGlidersClient.java
+++ fabric/src/main/java/com/evandev/reliable_gliders/client/ReliableGlidersClient.java
@@ -1,18 +1,50 @@
 package com.evandev.reliable_gliders.client;
 
+import com.evandev.reliable_gliders.Constants;
 import com.evandev.reliable_gliders.api.GlidingState;
 import com.evandev.reliable_gliders.network.SetGliderStatePayload;
 import com.evandev.reliable_gliders.network.SyncGliderSettingsPayload;
+import com.evandev.reliable_gliders.registry.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.component.DyedItemColor;
 
 public class ReliableGlidersClient implements ClientModInitializer {
 
+    private static boolean wasJumpKeyDown = false;
+    private static boolean wasOnGroundLastTick = true;
+
     @Override
     public void onInitializeClient() {
-        KeyMappingHelper.registerKeyMapping(ClientConstants.DEPLOY_KEY);
+        ModelLoadingPlugin.register(pluginContext -> {
+            pluginContext.addModels(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "item/glider_3d"));
+        });
+
+        ItemProperties.register(ModItems.GLIDER, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "gliding"),
+                (stack, level, entity, seed) -> {
+                    if (entity instanceof Player player && GlidingState.isGliding(player)) {
+                        return 1.0F;
+                    }
+                    return 0.0F;
+                });
+
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
+            if (tintIndex == 0) {
+                DyedItemColor color = stack.get(DataComponents.DYED_COLOR);
+                return color != null ? (0xFF000000 | color.rgb()) : -1;
+            }
+            return -1;
+        }, ModItems.GLIDER);
+
+        KeyBindingHelper.registerKeyBinding(ClientConstants.DEPLOY_KEY);
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
@@ -25,13 +57,23 @@ public class ReliableGlidersClient implements ClientModInitializer {
                 }
             }
 
-            while (ClientConstants.DEPLOY_KEY.consumeClick()) {
-                boolean newState = !GlidingState.wasGliding(client.player);
-                GlidingState.setGliding(client.player, newState);
-                if (ClientPlayNetworking.canSend(SetGliderStatePayload.TYPE)) {
-                    ClientPlayNetworking.send(new SetGliderStatePayload(newState));
+            while (ClientConstants.DEPLOY_KEY.consumeClick()) {}
+
+            boolean isJumpOrDeployDown = ClientConstants.DEPLOY_KEY.isDown() || client.options.keyJump.isDown();
+            boolean isOnGroundNow = client.player.onGround();
+
+            if (isJumpOrDeployDown && !wasJumpKeyDown) {
+                if (!wasOnGroundLastTick && !isOnGroundNow) {
+                    boolean newState = !GlidingState.wasGliding(client.player);
+                    GlidingState.setGliding(client.player, newState);
+                    if (ClientPlayNetworking.canSend(SetGliderStatePayload.TYPE)) {
+                        ClientPlayNetworking.send(new SetGliderStatePayload(newState));
+                    }
                 }
             }
+
+            wasJumpKeyDown = isJumpOrDeployDown;
+            wasOnGroundLastTick = isOnGroundNow;
         });
     }
 }
--- neoforge/src/main/java/com/evandev/reliable_gliders/client/ReliableGlidersClient.java
+++ neoforge/src/main/java/com/evandev/reliable_gliders/client/ReliableGlidersClient.java
@@ -1,33 +1,77 @@
 package com.evandev.reliable_gliders.client;
 
+import com.evandev.reliable_gliders.Constants;
 import com.evandev.reliable_gliders.api.GlidingState;
 import com.evandev.reliable_gliders.client.integration.ClothConfigIntegration;
 import com.evandev.reliable_gliders.network.SetGliderStatePayload;
 import com.evandev.reliable_gliders.network.SyncGliderSettingsPayload;
 import com.evandev.reliable_gliders.platform.Services;
+import com.evandev.reliable_gliders.registry.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.component.DyedItemColor;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.ModelEvent;
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.client.network.ClientPacketDistributor;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 public class ReliableGlidersClient {
+    private static boolean wasJumpKeyDown = false;
+    private static boolean wasOnGroundLastTick = true;
 
     public static void register(ModContainer container, IEventBus modEventBus) {
         if (Services.PLATFORM.isModLoaded("cloth_config")) {
             container.registerExtensionPoint(IConfigScreenFactory.class, (c, parent) -> ClothConfigIntegration.createScreen(parent));
         }
+
+        modEventBus.addListener(ReliableGlidersClient::onModelRegister);
+        modEventBus.addListener(ReliableGlidersClient::onClientSetup);
+        modEventBus.addListener(ReliableGlidersClient::onItemColors);
+        modEventBus.addListener(ReliableGlidersClient::registerKeyMappings);
+
+        NeoForge.EVENT_BUS.addListener(ReliableGlidersClient::onClientTick);
     }
 
-    @SubscribeEvent
+    public static void onModelRegister(ModelEvent.RegisterAdditional event) {
+        event.register(ModelResourceLocation.standalone(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "item/glider_3d")));
+    }
+
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            ItemProperties.register(ModItems.GLIDER, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "gliding"),
+                    (stack, level, entity, seed) -> {
+                        if (entity instanceof Player player && GlidingState.isGliding(player)) {
+                            return 1.0F;
+                        }
+                        return 0.0F;
+                    });
+        });
+    }
+
+    public static void onItemColors(RegisterColorHandlersEvent.Item event) {
+        event.register((stack, tintIndex) -> {
+            if (tintIndex == 0) {
+                DyedItemColor color = stack.get(DataComponents.DYED_COLOR);
+                return color != null ? (0xFF000000 | color.rgb()) : -1;
+            }
+            return -1;
+        }, ModItems.GLIDER);
+    }
+
     public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(ClientConstants.DEPLOY_KEY);
     }
 
-    @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
         Minecraft mc = Minecraft.getInstance();
         if (mc.player == null) return;
@@ -35,13 +79,24 @@ public class ReliableGlidersClient {
         if (mc.player.tickCount % 20 == 0) {
             boolean currentBoundState = !ClientConstants.DEPLOY_KEY.isUnbound();
             GlidingState.setKeyBound(mc.player, currentBoundState);
-            ClientPacketDistributor.sendToServer(new SyncGliderSettingsPayload(currentBoundState));
+            PacketDistributor.sendToServer(new SyncGliderSettingsPayload(currentBoundState));
         }
 
         while (ClientConstants.DEPLOY_KEY.consumeClick()) {
-            boolean newState = !GlidingState.wasGliding(mc.player);
-            GlidingState.setGliding(mc.player, newState);
-            ClientPacketDistributor.sendToServer(new SetGliderStatePayload(newState));
         }
+
+        boolean isJumpOrDeployDown = ClientConstants.DEPLOY_KEY.isDown() || mc.options.keyJump.isDown();
+        boolean isOnGroundNow = mc.player.onGround();
+
+        if (isJumpOrDeployDown && !wasJumpKeyDown) {
+            if (!wasOnGroundLastTick && !isOnGroundNow) {
+                boolean newState = !GlidingState.wasGliding(mc.player);
+                GlidingState.setGliding(mc.player, newState);
+                PacketDistributor.sendToServer(new SetGliderStatePayload(newState));
+            }
+        }
+
+        wasJumpKeyDown = isJumpOrDeployDown;
+        wasOnGroundLastTick = isOnGroundNow;
     }
 }
